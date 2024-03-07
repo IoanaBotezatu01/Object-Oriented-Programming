6f8ebb3f31c63c97d3cca0f8c7b0abf1de19a8d3
Ioana Botezatu <115404201+IoanaBotezatu01@users.noreply.github.com>
2024-03-07 17:21:25 +0200

    Add files via upload

---
+++ A8-OOP/Validator.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Exceptions.h"
+#include <string>
+
+class Validator
+{
+public:
+	static int is_number(std::string number);
+	static int valid_age(std::string age);
+	static void valid_name(std::string name);
+	static void valid_breed(std::string breed);
+	static void validateDog(std::string breed, std::string name, std::string age, std::string photograph);
+};
+
